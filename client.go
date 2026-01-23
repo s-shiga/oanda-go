d5cd7d993d78e8c20a64255d975d5c488f687f26
Satoshi Shiga <shiga800939@gmail.com>
2026-01-23 13:17:55 +0900

    add client.go

---
+++ client.go
@@ -0,0 +1,43 @@
+package oanda
+
+import (
+	"errors"
+	"os"
+)
+
+const (
+	fxTradeURL                  = "https://api-fxtrade.oanda.com"
+	fxTradePracticeURL          = "https://api-fxpractice.oanda.com"
+	fxTradeStreamingURL         = "https://stream-fxtrade.oanda.com/"
+	fxTradeStreamingPracticeURL = "https://stream-fxpractice.oanda.com/"
+)
+
+type Client struct {
+	URL          string
+	StreamingURL string
+	APIKey       string
+}
+
+func NewClient() (*Client, error) {
+	apiKey, ok := os.LookupEnv("OANDA_API_KEY")
+	if !ok {
+		return nil, errors.New("OANDA_API_KEY not set")
+	}
+	return &Client{
+		URL:          fxTradeURL,
+		StreamingURL: fxTradeStreamingURL,
+		APIKey:       apiKey,
+	}, nil
+}
+
+func NewPracticeClient() (*Client, error) {
+	apiKey, ok := os.LookupEnv("OANDA_API_KEY")
+	if !ok {
+		return nil, errors.New("OANDA_API_KEY not set")
+	}
+	return &Client{
+		URL:          fxTradePracticeURL,
+		StreamingURL: fxTradeStreamingPracticeURL,
+		APIKey:       apiKey,
+	}, nil
+}
