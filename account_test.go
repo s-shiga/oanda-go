ed90c3f737a5ca008a03947b5865f930f338e62e
Satoshi Shiga <shiga800939@gmail.com>
2026-01-25 22:53:39 +0900

    add test for AccountSummary
    add setup functions

--- account_test.go
+++ account_test.go
@@ -5,11 +5,24 @@ import (
 	"testing"
 )
 
-func TestClient_AccountsList(t *testing.T) {
+func setupClient(t *testing.T) *Client {
 	client, err := NewClient()
 	if err != nil {
 		t.Fatal(err)
 	}
+	return client
+}
+
+func setupAccountID(t *testing.T) AccountID {
+	accountID, ok := os.LookupEnv("OANDA_ACCOUNT_ID")
+	if !ok {
+		t.Fatal("OANDA_ACCOUNT_ID not set")
+	}
+	return AccountID(accountID)
+}
+
+func TestClient_AccountsList(t *testing.T) {
+	client := setupClient(t)
 	accounts, err := client.AccountList()
 	if err != nil {
 		t.Errorf("failed to list accounts: %v", err)
@@ -20,18 +33,23 @@ func TestClient_AccountsList(t *testing.T) {
 }
 
 func TestClient_AccountDetails(t *testing.T) {
-	client, err := NewClient()
-	if err != nil {
-		t.Fatal(err)
-	}
-	accountID, ok := os.LookupEnv("OANDA_ACCOUNT_ID")
-	if !ok {
-		t.Skip("OANDA_ACCOUNT_ID environment variable not set")
-	}
-	account, lastTransactionID, err := client.AccountDetails(AccountID(accountID))
+	client := setupClient(t)
+	accountID := setupAccountID(t)
+	account, lastTransactionID, err := client.AccountDetails(accountID)
 	if err != nil {
 		t.Errorf("failed to get account details: %v", err)
 	}
 	t.Logf("%#v", account)
+	t.Logf("lastTransactionID: %v", lastTransactionID)
+}
+
+func TestClient_AccountSummary(t *testing.T) {
+	client := setupClient(t)
+	accountID := setupAccountID(t)
+	accountSummary, lastTransactionID, err := client.AccountSummary(accountID)
+	if err != nil {
+		t.Errorf("failed to get account summary: %v", err)
+	}
+	t.Logf("%#v", accountSummary)
 	t.Logf("lastTransactionID: %v", lastTransactionID)
 }
